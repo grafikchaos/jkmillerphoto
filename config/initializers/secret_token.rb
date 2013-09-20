f7834885f6827eb67ebb63034953e80cba9014a4
Josh Johnson <josh@augustash.com>
2013-09-19 19:50:49 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JKMillerPhoto::Application.config.secret_key_base = 'f6ed581bb2a28790f136ca32af61faf9710774f68003f5c44cdcc10e1184c1790ad220ad623814683c773f049964f3922c16e1eeb3f703dfaf77ac14e4f85bf2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JKMillerPhoto::Application.config.session_store :cookie_store, key: '_JKMillerPhoto_session'
