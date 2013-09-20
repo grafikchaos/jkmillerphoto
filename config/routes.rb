dafce313469ae946b9a5a60e6e5ca35213f77f32
Josh Johnson <josh@augustash.com>
2013-09-19 21:25:59 -0500

    basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 JKMillerPhoto::Application.routes.draw do
+  # EXAMPLES
+  # resources :products
+  # root :to => 'welcome#index'
+
+  # MORE INFORMATION
+  # Run "rake routes" to see the routes available
+  # http://guides.rubyonrails.org/routing.html
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
   root :to => "home#index"
+
   devise_for :users
 end
