057dd99d592340e8fe4b73df84713a5069396f8b
Josh Johnson <josh@augustash.com>
2013-09-21 11:32:56 -0500

    more tweaking of puma configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec puma -p 5000 -C config/puma.rb -b unix:///tmp/puma-jkmillerphoto.sock
+web: bundle exec puma -p 5000 -C config/puma.rb
--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,14 @@
 environment ENV['RAILS_ENV']
+
+pidfile 'tmp/pids/puma.pid'
+state_path 'tmp/pids/puma.state'
+bind 'unix:///tmp/puma-jkmillerphoto.sock'
+stdout_redirect 'log/puma.log', 'log/puma_err.log'
+
 threads 1, 6
 workers 3
+
+
 preload_app!
 
 on_worker_boot do
