811efe70b59e75187d77a8533ae5a53b00f6d7e0
Josh Johnson <josh@augustash.com>
2013-09-21 11:13:13 -0500

    update puma configuration for binding unix socket

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,5 @@
 environment ENV['RAILS_ENV']
 threads 1, 6
-
 workers 3
 preload_app!
 
@@ -9,13 +8,3 @@ on_worker_boot do
     ActiveRecord::Base.establish_connection
   end
 end
-
-# workers 1
-# on_worker_boot do
-#   require "active_record"
-#   cwd = File.dirname(__FILE__)+"/.."
-#   puts cwd
-#   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-#   ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[environment])
-#   ActiveRecord::Base.verify_active_connections!
-# end
